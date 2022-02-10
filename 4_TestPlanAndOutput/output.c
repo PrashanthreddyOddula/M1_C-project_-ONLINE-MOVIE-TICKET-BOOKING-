6ca4902894affd5dc0d400835526819c9d981c49
PrashanthreddyOddula <63446991+PrashanthreddyOddula@users.noreply.github.com>
2022-02-10 19:46:39 +0530

    Create output.c

---
+++ 4_TestPlanAndOutput/output.c
@@ -0,0 +1,4 @@
+![output1](https://user-images.githubusercontent.com/63446991/153424981-f974392c-d331-4eba-be4f-21f0bdc31bcb.png)
+![output2](https://user-images.githubusercontent.com/63446991/153425196-6f8726ea-7c57-4f14-80c9-e700e1328043.png)
+![output3](https://user-images.githubusercontent.com/63446991/153425365-7f083409-2fcf-4851-96fd-129cb7b5499c.png)
+![output4](https://user-images.githubusercontent.com/63446991/153425616-50828213-a0c2-4e56-8869-dc8f72e1c8a6.png)
